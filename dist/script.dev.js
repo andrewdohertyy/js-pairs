060249ea79f007eed20d393896cdaece29165b9f
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-08-25 09:57:47 +0100

    added js variables and displayed the cards

---
+++ dist/script.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// set up an array containing all the cards needed for the game. 20 in total
+var cardLibrary = {
+  cards: [image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image]
+};
---
+++ script.js
@@ -0,0 +1,13 @@
+// set up an array containing all the cards needed for the game. 20 in total
+const cardLibrary = {
+    images: [
+        image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image,
+    ]
+}
+
+const cards = document.getElementsByClassName("card");
+const container = document.querySelector(".container");
+const timer = document.querySelector("#timer");
+const again = document.querySelector(".end__playAgain");
+const endGame = document.querySelector(".end");
+
