4810d9d87558358f5e6fec4b73d24baf55daa5d2
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-08-26 12:09:27 +0100

    added timer and display. Improved speed of clicks

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,6 +1,75 @@
 "use strict";
 
-// set up an array containing all the cards needed for the game. 20 in total
-var cardLibrary = {
-  cards: [image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image]
+var cards = document.querySelectorAll(".container__card");
+var container = document.querySelector(".container");
+var again = document.querySelector(".end__play-again");
+var endGame = document.querySelector(".end");
+var startButton = document.querySelector(".heading__button");
+var scoreTotal = document.querySelector(".heading__score");
+var timeDisplay = document.querySelector(".heading__timer");
+var timeStamp = document.querySelector('.end__timestamp');
+var first;
+var second;
+var timer;
+var matchCounter = 0;
+
+var startTimer = function startTimer() {
+  var sec = 0;
+  timer = setInterval(function () {
+    timeDisplay.innerHTML = sec;
+    sec++;
+  }, 1000);
+}; //when the start button is clicked it will hide and generate a random order of the array
+
+
+var randomStart = function randomStart() {
+  container.classList.remove('display');
+  startButton.classList.add('display');
+  endGame.classList.add('display');
+  timeDisplay.classList.remove('display');
+  cards.forEach(function (card) {
+    var randomNum = Math.floor(Math.random() * 24);
+    card.style.order = randomNum;
+    startTimer();
+  });
 };
+
+again.addEventListener("click", randomStart);
+startButton.addEventListener("click", randomStart);
+cards.forEach(function (card) {
+  card.addEventListener('click', function () {
+    if (!first && !second) {
+      first = card;
+      card.classList.add('show');
+    } else if (first && !second) {
+      second = card;
+      card.classList.add('show');
+    }
+
+    if (first.innerHTML === second.innerHTML) {
+      first.style.pointerEvents = 'none';
+      second.style.pointerEvents = 'none';
+      first = undefined;
+      second = undefined;
+      matchCounter++;
+    }
+
+    if (matchCounter >= 12) {
+      container.classList.add('display');
+      endGame.classList.remove('display');
+      timeDisplay.classList.add('display');
+      timeStamp.innerHTML = "You completed the puzzle in ".concat(timeDisplay.innerHTML, " seconds! Well done!");
+    } else {
+      first.classList.add('hide');
+      second.classList.add('hide');
+      setTimeout(function () {
+        first.classList.remove('show');
+        second.classList.remove('show');
+        first.classList.remove('hide');
+        second.classList.remove('hide');
+        first = undefined;
+        second = undefined;
+      }, 500);
+    }
+  });
+});
--- script.js
+++ script.js
@@ -1,13 +1,75 @@
-// set up an array containing all the cards needed for the game. 20 in total
-const cardLibrary = {
-    images: [
-        image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image, image,
-    ]
+const cards = document.querySelectorAll(".container__card");
+const container = document.querySelector(".container");
+const again = document.querySelector(".end__play-again");
+const endGame = document.querySelector(".end");
+const startButton = document.querySelector(".heading__button");
+const scoreTotal = document.querySelector(".heading__score");
+const timeDisplay = document.querySelector(".heading__timer");
+const timeStamp = document.querySelector('.end__timestamp')
+
+let first;
+let second;
+let timer;
+let matchCounter = 0;
+
+const startTimer = () => {
+    let sec = 0;
+    timer = setInterval(() => {
+        timeDisplay.innerHTML =  sec;
+        sec ++
+    }, 1000)
 }
 
-const cards = document.getElementsByClassName("card");
-const container = document.querySelector(".container");
-const timer = document.querySelector("#timer");
-const again = document.querySelector(".end__playAgain");
-const endGame = document.querySelector(".end");
+//when the start button is clicked it will hide and generate a random order of the array
+const randomStart = () => {
+    container.classList.remove('display')
+    startButton.classList.add('display')
+    endGame.classList.add('display');
+    timeDisplay.classList.remove('display')
+    cards.forEach((card) => {
+    let randomNum = Math.floor(Math.random() * 24);        
+    card.style.order = randomNum;
+    startTimer();
+})}
 
+again.addEventListener("click", randomStart);
+
+startButton.addEventListener("click", randomStart);
+
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        if (!first && !second) {
+            first = card;
+            card.classList.add('show');
+        } else if (first && !second) {
+            second = card;
+            card.classList.add('show');
+        }
+        if (first.innerHTML === second.innerHTML) {
+            first.style.pointerEvents = 'none';
+            second.style.pointerEvents = 'none';
+            first = undefined;
+            second = undefined;
+            matchCounter ++;
+        }
+
+        if (matchCounter >= 12) {
+            container.classList.add('display');
+            endGame.classList.remove('display');
+            timeDisplay.classList.add('display');
+            timeStamp.innerHTML = `You completed the puzzle in ${timeDisplay.innerHTML} seconds! Well done!`
+        }  else {
+            first.classList.add('hide');
+            second.classList.add('hide');
+            
+            setTimeout(() => {
+            first.classList.remove('show');
+            second.classList.remove('show');
+            first.classList.remove('hide');
+            second.classList.remove('hide');
+            first = undefined;
+            second = undefined;
+            }, 500);
+        }
+    })
+});
